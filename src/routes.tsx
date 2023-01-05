86ffb7dd07c9fbc6677f4a6bb32ea8c11623ae5e
rafaeuus <rafaeljcarvalhosilva@gmail.com>
2023-01-05 16:25:02 -0300

    fix: adding protected routes and creating profile route

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom";
+import ProtectedRoutes from "./components/ProtectedRoutes/ProtectedRoutes";
 import DashboardVisitorPage from "./pages/DashboardVisitorPage";
 import LoginPage from "./pages/LoginPage";
+import ProfileUserPage from "./pages/ProfileUserPage";
 import RegisterPage from "./pages/RegisterPage";
 
 export const RoutesComponent = () => {
@@ -9,6 +11,9 @@ export const RoutesComponent = () => {
       <Route path="/" element={<DashboardVisitorPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route element={<ProtectedRoutes />}>
+        <Route path="/profile" element={<ProfileUserPage />} />
+      </Route>
     </Routes>
   );
 };
