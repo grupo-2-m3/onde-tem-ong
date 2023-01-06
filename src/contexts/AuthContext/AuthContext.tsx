5ee1a0961488fffc7595cabe9b3f192be2809c47
rafaeuus <rafaeljcarvalhosilva@gmail.com>
2023-01-06 20:18:06 -0300

    fix: fixing registerPage form

--- src/contexts/AuthContext/AuthContext.tsx
+++ src/contexts/AuthContext/AuthContext.tsx
@@ -41,27 +41,27 @@ export const AuthProvider = ({ children }: iAuthProvider) => {
       const response = await api.post("/users", data);
       localStorage.clear();
       localStorage.setItem("USERID", response.data.accessToken);
-      if(response.data.user.type === "owner_ong"){
-        createOng(response.data.user)
+      if (response.data.user.type === "owner_ong") {
+        createOng(response.data.user);
       }
     } catch (error) {
       console.error(error);
     }
-  }
-  
+  };
+
   const createOng = async (data: any) => {
-      const newData = {
-        userID: data.id,
-        name: data.name,
-        contact: [{email: data.email}],  
-        category: data?.category,
-        bio: data?.bio,
-      }
-      const UserID = localStorage.getItem("USERID");
-      const response = await api.post("/ongs", newData, {
-        headers: {"Authorization": `Bearer ${UserID}`}
-      })
-  }
+    const newData = {
+      userID: data.id,
+      name: data.name,
+      contact: [{ email: data.email }],
+      category: data?.category,
+      bio: data?.bio,
+    };
+    const UserID = localStorage.getItem("USERID");
+    const response = await api.post("/ongs", newData, {
+      headers: { Authorization: `Bearer ${UserID}` },
+    });
+  };
 
   return (
     <AuthContext.Provider value={{ userLogin, registerSubmit }}>
