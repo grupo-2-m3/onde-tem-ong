71b8865e067b37d35a99c90dcf30b57ce1f11159
Rafael Carvalho <110257133+rafaeuus@users.noreply.github.com>
2023-01-07 15:49:58 -0300

    Update AuthContext.tsx

--- src/contexts/AuthContext/AuthContext.tsx
+++ src/contexts/AuthContext/AuthContext.tsx
@@ -44,7 +44,6 @@ export const AuthProvider = ({ children }: iAuthProvider) => {
       if (response.data.user.type === "owner_ong") {
         createOng(response.data.user);
       }
-      console.log("teste");
       navigate("/login");
     } catch (error) {
       console.error(error);
