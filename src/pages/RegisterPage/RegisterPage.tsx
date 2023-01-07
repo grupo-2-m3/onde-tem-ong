7ef8c1ac9ca7ec07b8f61b3cb9fe9927015c3c74
Lhuana Lee <leelhuana@gmail.com>
2023-01-06 21:48:57 -0300

    fix: styling inputs and updating RegisterPage

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -10,145 +10,235 @@ import UserLight from "../../assets/imgs/User Light.svg";
 import PasswordLight from "../../assets/imgs/Password Light.svg";
 import PencilLight from "../../assets/imgs/Pencil Light.svg";
 import LogoWhite from "../../assets/imgs/LogoWhite.svg";
+import { RiErrorWarningLine } from "react-icons/ri";
+import { Error } from "../../components/Input/StyledInput";
 
 export interface iRegisterData {
-    email: string;
-    name: string;
-    password: string;
-    confirmPassword: string;
-    type: string;
-    category: string;
-    bio: string;
-}     
+  email: string;
+  name: string;
+  password: string;
+  confirmPassword: string;
+  type: string;
+  category: string;
+  bio: string;
+}
 
 const RegisterPage = () => {
+  const [typeUser, setTypeUser] = useState("");
 
-    const [typeUser, setTypeUser] = useState("");   
+  const { registerSubmit } = useContext(AuthContext);
 
-    const {registerSubmit} = useContext(AuthContext);
+  const RegisterSchema = yup.object().shape({
+    name: yup.string().required("O nome é obrigatório"),
+    email: yup.string().required("O email é obrigatório"),
+    password: yup.string().required("A senha é obrigatória"),
+    confirmPassword: yup
+      .string()
+      .required("A confirmação de senha é obrigatória")
+      .oneOf([yup.ref("password")], "As senhas precisam coincidir"),
+    type: yup.string().required("O tipo é obrigatório"),
+    category: yup.string().required("A categoria é obrigatória"),
+    bio: yup.string().required("A descrição é obrigatória"),
+  });
 
-    const RegisterSchema = yup.object().shape({
-        name: yup.string().required("O nome é obrigatório"),
-        email: yup.string().required("O email é obrigatório"),
-        password: yup.string().required("A senha é obrigatória"),
-        confirmPassword: yup.string().oneOf([yup.ref("password")], "As senhas precisam coincidir"),
-        type: yup.string().required("O tipo é obrigatório"),
-        category: yup.string(),
-        bio: yup.string(),
-    })
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<iRegisterData>({
+    resolver: yupResolver(RegisterSchema),
+  });
 
-    const { register, handleSubmit } = useForm<iRegisterData>({
-        resolver: yupResolver(RegisterSchema)
-    })
-
-    return (
-      <StyledRegisterPage>
-        <div className="background">
-          <main className="container">
+  return (
+    <StyledRegisterPage>
+      <div className="background">
+        <main className="container">
+          <div className="formContainer">
             <div className="RegisterAndTitle">
               <div className="RegisterMobile">
                 <h1 className="titleRegister">Cadastro</h1>
               </div>
             </div>
-            <div className="formContainer">
-              <form onSubmit={handleSubmit(registerSubmit)}>
-                <div className="divBlockLeft">
-                    <Input
-                      styledColor="Withe" 
-                      label="EMAIL"
-                      src={UserLight} 
-                      id="email" 
-                      type="email" 
-                      placeholder="Digite aqui seu email" 
-                      {...register("email")}/>
-                    <Input 
-                      styledColor="Withe"
-                      label="NOME - USUÁRIO OU ONG" 
-                      src={PencilLight}
-                      id="name" 
-                      type="text" 
-                      placeholder="Digite aqui seu nome" 
-                      {...register("name")}/>
-                    <Input
-                      styledColor="Withe"
-                      label="SENHA"
-                      src={PasswordLight} 
-                      id="password" 
-                      type="password" 
-                      placeholder="Digite aqui sua senha" 
-                      {...register("password")}/>
-                    <Input 
-                      styledColor="Withe"
-                      label="REPITA A SENHA" 
-                      src={PasswordLight}
-                      id="confirmPassword" 
-                      type="password" 
-                      placeholder="Confirme sua senha" 
-                      {...register("confirmPassword")}/>
-                    <label className="labelSelectType" htmlFor="">SELECIONE TIPO DE USUÁRIO</label>
-                    <div className="selectTypeUser">
+            <form onSubmit={handleSubmit(registerSubmit)}>
+              <div className="divBlockLeft">
+                <div
+                  className={
+                    errors.email ? "inputContainer error" : "inputContainer"
+                  }
+                >
+                  <Input
+                    styledColor="Withe"
+                    label="EMAIL"
+                    src={UserLight}
+                    id="email"
+                    type="email"
+                    placeholder="Digite aqui seu email"
+                    {...register("email")}
+                  />
+                  {errors.email && (
+                    <Error>
+                      <RiErrorWarningLine size="20" className="ico" />
+                      <span>{errors.email.message}</span>
+                    </Error>
+                  )}
+                </div>
+                <div
+                  className={
+                    errors.name ? "inputContainer error" : "inputContainer"
+                  }
+                >
+                  <Input
+                    styledColor="Withe"
+                    label="NOME - USUÁRIO OU ONG"
+                    src={PencilLight}
+                    id="name"
+                    type="text"
+                    placeholder="Digite aqui seu nome"
+                    {...register("name")}
+                  />
+                  {errors.name && (
+                    <Error>
+                      <RiErrorWarningLine size="20" className="ico" />
+                      <span>{errors.name.message}</span>
+                    </Error>
+                  )}
+                </div>
+                <div
+                  className={
+                    errors.password ? "inputContainer error" : "inputContainer"
+                  }
+                >
+                  <Input
+                    styledColor="Withe"
+                    label="SENHA"
+                    src={PasswordLight}
+                    id="password"
+                    type="password"
+                    placeholder="Digite aqui sua senha"
+                    {...register("password")}
+                  />
+                  {errors.password && (
+                    <Error>
+                      <RiErrorWarningLine size="20" className="ico" />
+                      <span>{errors.password.message}</span>
+                    </Error>
+                  )}
+                </div>
+                <div
+                  className={
+                    errors.confirmPassword
+                      ? "inputContainer error"
+                      : "inputContainer"
+                  }
+                >
+                  <Input
+                    styledColor="Withe"
+                    label="REPITA A SENHA"
+                    src={PasswordLight}
+                    id="confirmPassword"
+                    type="password"
+                    placeholder="Confirme sua senha"
+                    {...register("confirmPassword")}
+                  />
+                  {errors.confirmPassword && (
+                    <Error>
+                      <RiErrorWarningLine size="20" className="ico" />
+                      <span>{errors.confirmPassword.message}</span>
+                    </Error>
+                  )}
+                </div>
+
+                <label className="labelSelectType" htmlFor="">
+                  SELECIONE TIPO DE USUÁRIO
+                </label>
+                <div className="selectTypeUser">
+                  <Input
+                    styledColor="placeholderInputRegister"
+                    className="inputRadio"
+                    src={UserLight}
+                    label="Usuário"
+                    id="user"
+                    type="radio"
+                    value="user"
+                    {...register("type")}
+                    onChange={(event) => setTypeUser(event.target.value)}
+                  />
+                  <Input
+                    styledColor="placeholderInputRegister"
+                    className="inputRadio"
+                    src={UserLight}
+                    label="CEO ONG"
+                    id="owner_ong"
+                    type="radio"
+                    value="owner_ong"
+                    {...register("type")}
+                    onChange={(event) => setTypeUser(event.target.value)}
+                  />
+                </div>
+              </div>
+              <div className="divBlockRight">
+                {typeUser === "owner_ong" ? (
+                  <div className="divSelectCategory">
+                    <div className="LabelAndSelect">
+                      <label className="labelSelectCategory" htmlFor="category">
+                        SELECIONE A CATEGORIA
+                      </label>
+                      <select id="category" {...register("category")}>
+                        <option value="">Sem categoria</option>
+                        <option value="seguranca alimentar">
+                          Segurança Alimentar
+                        </option>
+                        <option value="Cultura">Cultura</option>
+                        <option value="Saúde">Saúde</option>
+                        <option value="Meio Ambiente">Meio Ambiente</option>
+                        <option value="Desenvolvimento e defesa de direitos">
+                          Desenvolvimento e defesa de direitos
+                        </option>
+                        <option value="Habitação">Habitação</option>
+                        <option value="Animais">Animais</option>
+                        <option value="Povos nativos">Povos nativos</option>
+                        <option value="Tecnologia">Tecnologia</option>
+                        <option value="Animais">Animais</option>
+                      </select>
+                    </div>
+                    <div
+                      className={
+                        errors.bio ? "inputContainer error" : "inputContainer"
+                      }
+                    >
                       <Input
-                        styledColor="placeholderInputRegister"
-                        className="inputRadio"
-                        src={UserLight} 
-                        label="Usuário" 
-                        id="user" 
-                        type="radio" 
-                        value="user" 
-                        {...register("type")} 
-                        onChange={(event) => setTypeUser(event.target.value)}/>
-                      <Input 
-                        styledColor="placeholderInputRegister"
-                        className="inputRadio"
-                        src={UserLight} 
-                        label="CEO ONG" 
-                        id="owner_ong" 
-                        type="radio" 
-                        value="owner_ong" 
-                        {...register("type")} 
-                        onChange={(event) => setTypeUser(event.target.value)}/>
+                        styledColor="Grey100"
+                        label="BIO"
+                        src={PencilLight}
+                        id="bio"
+                        type="text"
+                        placeholder="Descreva sua ONG"
+                        {...register("bio")}
+                      />
+                      {errors.bio && (
+                        <Error>
+                          <RiErrorWarningLine size="20" className="ico" />
+                          <span>{errors.bio.message}</span>
+                        </Error>
+                      )}
                     </div>
                   </div>
-                  <div className="divBlockRight">
-                  {typeUser === "owner_ong" ?
-                      <div className="divSelectCategory">
-                          <div className="LabelAndSelect">
-                            <label className="labelSelectCategory" htmlFor="category">SELECIONE A CATEGORIA</label>
-                            <select id="category" {...register("category")}>
-                              <option value="">Sem categoria</option>
-                              <option value="seguranca alimentar">Segurança Alimentar</option>
-                              <option value="Cultura">Cultura</option>
-                              <option value="Saúde">Saúde</option>
-                              <option value="Meio Ambiente">Meio Ambiente</option>
-                              <option value="Desenvolvimento e defesa de direitos">Desenvolvimento e defesa de direitos</option>
-                              <option value="Habitação">Habitação</option>
-                              <option value="Animais">Animais</option>
-                              <option value="Povos nativos">Povos nativos</option>
-                              <option value="Tecnologia">Tecnologia</option>
-                              <option value="Animais">Animais</option>
-                          </select>
-                          </div>
-                          <Input 
-                            styledColor="Grey100"
-                            label="BIO" 
-                            src={PencilLight}
-                            id="bio" 
-                            type="text" 
-                            placeholder="Descreva sua ONG" 
-                            {...register("bio")}/>   
-                      </div> : ""
-                  }
-                  <button type="submit">Cadastrar</button>
+                ) : (
+                  ""
+                )}
+                <button type="submit">Cadastrar</button>
                 <span>Já possui uma conta?</span>
-                <button type="button" className="LoginButton">Login</button>
-                <img src={LogoWhite} alt=""/>
-                </div>
-              </form>
-            </div>
-          </main>
-        </div>
-      </StyledRegisterPage>
-    )
-}
+                <button type="button" className="LoginButton">
+                  Login
+                </button>
+                <img src={LogoWhite} alt="" />
+              </div>
+            </form>
+          </div>
+        </main>
+      </div>
+    </StyledRegisterPage>
+  );
+};
 
 export default RegisterPage;
