3c94041dd5dc121685683fb9f2e4bad5d8fdc600
Fernanda Manfrin <fernandamanfrini@hotmail.com>
2023-01-09 12:06:53 -0300

    feat: creat request getDonateUser

--- src/contexts/UserContext/UserContext.tsx
+++ src/contexts/UserContext/UserContext.tsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { api } from "../../services/api";
 
 interface iUserProvider {
@@ -24,15 +24,34 @@ export interface iDonateOng {
   value: number;
   id: number;
 }
+export interface iDonateUser {
+  userId: number;
+  ongId: number;
+  public: boolean;
+  value: number;
+  id: number;
+}
+
 
 export const UserContext = createContext({} as iUserContextProps);
 
 export const UserProvider = ({ children }: iUserProvider) => {
+  const [userDonate, setUserDonate] = useState({} as iDonateUser);
 
   const getAllOngs = async () => {
     const allOngs = await api.get<iOng[]>("/ongs");
     return allOngs;
   };
+
+  const getDonateUser = async()=>{
+    const token = localStorage.getItem("@token");
+    const id = localStorage.getItem("@id");
+    const donateUser = await api.get<iDonateUser[]>(`/users/donate/${id}`,{
+      headers: { Authorization: `Bearer ${token}` },
+    })
+
+    return donateUser
+  }
 
   return (
     <UserContext.Provider value={{}}>{children}</UserContext.Provider>
