d42088d5d16b02e7974194868a7dcf5292cf13bb
Rafael Carvalho <110257133+rafaeuus@users.noreply.github.com>
2023-01-11 19:34:09 -0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import App from "./App";
 import { Providers } from "./contexts/Provider";
 import { GlobalStyle } from "./styles/GlobalStyle";
 import "swiper/css/bundle";
-import "../src/pages/DashboardVisitorPage/Carousel/styles.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
